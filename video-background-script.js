29cb94e903f2cf4d75506097c0f8e235eb666092
Nicholas Britt <Nicholas Britt>
2016-05-16 20:22:33 -0500

    Added transitions for videos on landing page background

--- video-background-script.js
+++ video-background-script.js
@@ -84,14 +84,14 @@
 /*
 notes:
 
-var myPlayer = document.getElementById('video-background');
-var activeVideo = document.getElementById('active-video');
+var player1 = document.getElementById('video-background1');
+var activeVideo1 = document.getElementById('active-video1');
 
-activeVideo.src = myvideos[index].url;
-myPlayer.load();
-myPlayer.play();
+activeVideo1.src = myvideos[index].url;
+player1.load();
+player1.play();
 
-myPlayer.removeEventListener('ended', listener?listener:0);
+player1.removeEventListener('ended', listener?listener:0);
 
 
 Assignment:
@@ -105,38 +105,77 @@ in video element on homepage.
 Don't worry about auto advance for now.
 
 Call initialize task.
+set timeout for the duration of the video on timeout I call next video.
+
 
 
 */
 
     function initializeBackgroundVideos(videos) {
-        state.currentlyPlayingVideoIndex = 0
+        state.currentlyPlayingVideoIndex = 0;
         state.numberOfVideos = videos.length;
         state.randomizedVideos = randomizeVideos(videos);
-        state.myPlayer = document.getElementById('video-background');
-        state.activeVideo = document.getElementById('active-video');
+        state.player1 = document.getElementById('video-background1');
+        state.activeVideo1 = document.getElementById('active-video1');
+        state.player2 = document.getElementById('video-background2');
+        state.activeVideo2 = document.getElementById('active-video2');
 
-        loadVideo();
+        state.player1.addEventListener('loadeddata', onPlayer1LoadedData);
+        state.player2.addEventListener('loadeddata', onPlayer2LoadedData);
+        // state.player1.addEventListener('ended', nextVideo);
 
-        state.myPlayer.addEventListener('ended', nextVideo);
+        loadVideo(state.player1, state.activeVideo1);
+
     }
 
     initializeBackgroundVideos(videos);
 
-    function loadVideo() {
+    function loadVideo(player, activeVideo) {
         var url = state.randomizedVideos[state.currentlyPlayingVideoIndex].url;
-        state.activeVideo.src = url;
-        state.myPlayer.load();
-        state.myPlayer.play();
+        player.pause();
+        activeVideo.src = url;
+        player.load();
+        player.play();
     }
 
-    function nextVideo() {
+    function setNextVideoIndex(){
         state.currentlyPlayingVideoIndex  += 1;
-
         if (state.currentlyPlayingVideoIndex >= state.numberOfVideos) {
             state.currentlyPlayingVideoIndex = 0;
         }
-
-        loadVideo();
     }
+    function player1LoadNextVideo(){
+        setNextVideoIndex();
+        loadVideo(state.player2, state.activeVideo2);
+        showVideo2();
+
+    }
+    function player2LoadNextVideo(){
+        setNextVideoIndex();
+        loadVideo(state.player1, state.activeVideo1);
+        hideVideo2();
+
+    }
+
+    function showVideo2(){
+        state.player2.style.opacity = 1;
+    }
+    function hideVideo2(){
+        state.player2.style.opacity = 0;
+    }
+
+    function onPlayer1LoadedData() {
+        var currentlyPlayingVideoDuration = state.player1.duration;
+        var cutoffTime = (currentlyPlayingVideoDuration - 1) * 1000;
+        window.setTimeout(player1LoadNextVideo, cutoffTime);
+
+    }
+
+    function onPlayer2LoadedData() {
+        var currentlyPlayingVideoDuration = state.player2.duration;
+        var cutoffTime = (currentlyPlayingVideoDuration - 1) * 1000;
+        window.setTimeout(player2LoadNextVideo, cutoffTime);
+
+    }
+
 })();
